3ea522a5d0c25eac0d96684cc1537d002840889f
Balazs Kiss <dev@balazskiss.name>
2023-08-03 23:27:22 +0200

    apk browser

--- packages/apk-browser/main.js
+++ packages/apk-browser/main.js
@@ -1,4 +1,63 @@
 const { exec } = require("child_process");
+const fs = require('fs');
+const path = require('path');
+
+function getFilesAndSizesRecursive(directoryPath) {
+  return new Promise((resolve, reject) => {
+    let filesList = [];
+
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      let pending = files.length;
+      if (pending === 0) {
+        // If there are no files or directories in the current directory
+        resolve(filesList);
+        return;
+      }
+
+      files.forEach(file => {
+        const filePath = path.join(directoryPath, file);
+
+        fs.stat(filePath, (err, stats) => {
+          if (err) {
+            // Stat can return with error when the file is an alias.
+            filesList.push({ name: file, size: 0 });
+            pending--;
+            if (pending === 0) {
+              resolve(filesList);
+            }
+            return;
+          }
+
+          if (stats.isDirectory()) {
+            // If it's a directory, recursively call the function for subdirectory
+            getFilesAndSizesRecursive(filePath)
+              .then(subdirFilesList => {
+                filesList.push({ name: file, size: 0, files: subdirFilesList});
+                pending--;
+                if (pending === 0) {
+                  resolve(filesList);
+                }
+              })
+              .catch(reject);
+          } else if (stats.isFile()) {
+            // If it's a file, add file details to the list
+            filesList.push({ name: file, size: stats.size });
+
+            pending--;
+            if (pending === 0) {
+              resolve(filesList);
+            }
+          }
+        });
+      });
+    });
+  });
+}
 
 runCommand = function(command, callback) {
   exec(command, (error, stdout, stderr) => {
@@ -21,9 +80,15 @@ module.exports.getFileList = function (requestData, callback) {
     const tmpDirPath = result;
     console.log(tmpDirPath);
     runCommand(`unzip "${filePath}" -d "${tmpDirPath}"`, (result) => {
-      runCommand(`ls -laR "${tmpDirPath}"`, (result) => {
-        callback({text: result})
+      getFilesAndSizesRecursive(tmpDirPath)
+      .then(filesList => {
+        console.log("done");
+        console.log(filesList);
+        callback(filesList);
       })
+      .catch(err => {
+        console.error('Error:', err);
+      });
     })
   });
 }
